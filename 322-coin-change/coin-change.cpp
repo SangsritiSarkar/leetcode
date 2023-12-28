2405f403b0cfabf25814105b8abc2bed2dba2092
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2023-12-29 03:37:58 +0530

    Time: 63 ms (71.17%) | Memory: 14.8 MB (61.66%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -4,7 +4,7 @@ public:
 
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<int> prev(amount+1, 0), curr(amount+1, 0);
+        vector<int> prev(amount+1, 0);
         //BASE CASE
         for(int t=0;t<=amount;t++)
         {
@@ -18,10 +18,10 @@ public:
             {
                 int notTake=0+prev[target];
                 int take=1e9;
-                if(coins[ind]<=target) take=1+curr[target-coins[ind]];
-                curr[target]=min(take, notTake);
+                if(coins[ind]<=target) take=1+prev[target-coins[ind]];
+                prev[target]=min(take, notTake);
             }
-            prev=curr;
+            
         }
         if(prev[amount]>=1e9) return -1;
         return prev[amount];
