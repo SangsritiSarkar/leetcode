488bf339f4060e18c0566afaeb43327e65141bb7
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2023-12-28 14:26:02 +0530

    Time: 216 ms (9.01%) | Memory: 18.9 MB (37.90%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,50 +1,30 @@
 class Solution {
 public:
-    //MEMOIZATION
-    // int f(int ind,int target, vector<int>& coins, vector<vector<int>>& dp)
-    // {
-    //     if(ind==0)
-    //     {
-    //         if(target%coins[0]==0) return target/coins[0];
-    //         return 1e9;
-    //     }
-    //     if(dp[ind][target]!=-1) return dp[ind][target];
-    //     int notTake=0+f(ind-1,target,coins,dp);
-    //     int take=INT_MAX;
-    //     if(coins[ind]<=target) take=1+f(ind,target-coins[ind],coins,dp);
-    //     return dp[ind][target]=min(take, notTake);
-    // }
-    // int coinChange(vector<int>& coins, int amount) {
-    //     int n=coins.size();
-    //     vector<vector<int>> dp(n, vector<int> (amount+1, -1));
-    //     int ans= f(n-1,amount,coins,dp);
-    //     if(ans>=1e9) return -1;
-    //     return ans;
-    // }
+//SPACE OPTIMIZATION
 
-    //TABULATION
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1, 0));
+        vector<int> prev(amount+1, 0), curr(amount+1, 0);
         //BASE CASE
         for(int t=0;t<=amount;t++)
         {
-            if(t%coins[0]==0) dp[0][t]=t/coins[0];
-            else dp[0][t]=1e9;
+            if(t%coins[0]==0) prev[t]=t/coins[0];
+            else prev[t]=1e9;
         }
         //STATES
         for(int ind=1;ind < n;ind++)
         {
             for(int target=0;target <= amount;target++)
             {
-                int notTake=0+dp[ind-1][target];
+                int notTake=0+prev[target];
                 int take=1e9;
-                if(coins[ind]<=target) take=1+dp[ind][target-coins[ind]];
-                dp[ind][target]=min(take, notTake);
+                if(coins[ind]<=target) take=1+curr[target-coins[ind]];
+                curr[target]=min(take, notTake);
             }
+            prev=curr;
         }
-        if(dp[n-1][amount]>=1e9) return -1;
-        return dp[n-1][amount];
+        if(prev[amount]>=1e9) return -1;
+        return prev[amount];
         
     }
 };
