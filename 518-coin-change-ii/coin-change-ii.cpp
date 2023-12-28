57136875c74845ee363ac4a6c787648e2ae8dc66
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2023-12-29 02:09:12 +0530

    Time: 83 ms (9.98%) | Memory: 18.6 MB (61.78%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
 
-
-    int f(int ind, int tar, vector<int>& coins, vector<vector<int>>& dp)
-    {
-        if(ind==0)
-        {
-            if(tar%coins[0]==0) return 1;
-            return 0;
-        }
-        if(dp[ind][tar]!=-1) return dp[ind][tar];
-        int notTake=f(ind-1,tar,coins,dp);
-        int take=0;
-        if(coins[ind]<=tar) take=f(ind,tar-coins[ind], coins,dp);//not 1+..becoz it is no of ways
-        return dp[ind][tar]=take+notTake;
-    }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1, -1));
-        return f(n-1,amount,coins,dp);
+        vector<vector<int>> dp(n, vector<int> (amount+1, 0));
+        for(int tar=0;tar<=amount;tar++)
+        {
+            dp[0][tar]=(tar%coins[0]==0);
+        }
+        for(int ind=1;ind<n;ind++)
+        {
+            for(int tar=0;tar<=amount;tar++)
+            {
+                int notTake=dp[ind-1][tar];
+                int take=0;
+                if(coins[ind]<=tar) take=dp[ind][tar-coins[ind]];//not 1+..becoz it is no of ways
+                dp[ind][tar]=take+notTake;
+            }
+        }
+        return dp[n-1][amount];
+        
     }
 };
