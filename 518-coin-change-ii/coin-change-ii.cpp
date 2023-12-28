192b65fd6f54670e8f4f4449922a144fcd0ad726
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2023-12-29 02:12:28 +0530

    Time: 57 ms (11.71%) | Memory: 7.7 MB (75.19%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -3,22 +3,23 @@ public:
 
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n, vector<int> (amount+1, 0));
+        vector<int> prev(amount+1, 0), curr(amount+1, 0);
         for(int tar=0;tar<=amount;tar++)
         {
-            dp[0][tar]=(tar%coins[0]==0);
+            prev[tar]=(tar%coins[0]==0);
         }
         for(int ind=1;ind<n;ind++)
         {
             for(int tar=0;tar<=amount;tar++)
             {
-                int notTake=dp[ind-1][tar];
+                int notTake=prev[tar];
                 int take=0;
-                if(coins[ind]<=tar) take=dp[ind][tar-coins[ind]];//not 1+..becoz it is no of ways
-                dp[ind][tar]=take+notTake;
+                if(coins[ind]<=tar) take=curr[tar-coins[ind]];//not 1+..becoz it is no of ways
+                curr[tar]=take+notTake;
             }
+            prev=curr;
         }
-        return dp[n-1][amount];
+        return prev[amount];
         
     }
 };
