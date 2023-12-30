c89ca3f64a00be04d2a99c669c99601d80158c50
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2023-12-30 14:36:15 +0530

    Time: 38 ms (73.07%) | Memory: 6.8 MB (92.94%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -30,26 +30,43 @@ public:
     //     return f(n,m,text1,text2,dp);
     // }
 
-   //TABULATION
+   //SPACE OPTIMIZATION
     int longestCommonSubsequence(string s1, string s2) {
-        int n=s1.size();
-        int m=s2.size(); 
-        vector<vector<int>> dp(n+1, vector<int> (m+1,0));
-        for(int i=0;i<=n;i++) dp[i][0]=0;
-        for(int j=0;j<=m;j++) dp[0][j]=0;
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+        // int n=s1.size();
+        // int m=s2.size(); 
+        // vector<int> prev(n+1,0), curr(m+1,0);
+        // //for(int i=0;i<=n;i++) dp[i][0]=0; //column no neeed
+        // for(int j=0;j<=m;j++) prev[j]=0; 
+        // for(int i=1;i<=n;i++)
+        // {
+        //     for(int j=1;j<=m;j++)
+        //     {
+        //         if(s1[i-1]==s2[j-1]) curr[j]=1+prev[j-1];
+        //         else curr[j]=max(prev[j],curr[j-1]);
+        //     }
+        //     prev=curr;
+        // }
+        // return prev[m];
+        int n = s1.size();
+        int m = s2.size();
+
+        // Initialize two vectors to store the current and previous rows of the DP table
+        vector<int> prev(m + 1, 0), cur(m + 1, 0);
+
+        // Base case is covered as we have initialized the prev and cur vectors to 0.
+
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                if (s1[ind1 - 1] == s2[ind2 - 1])
+                    cur[ind2] = 1 + prev[ind2 - 1]; // Characters match, increment LCS length
+                else
+                    cur[ind2] = max(prev[ind2], cur[ind2 - 1]); // Characters don't match, consider the maximum from above or left
             }
+            prev = cur; // Update the previous row with the current row
         }
-        return dp[n][m];
+
+        return prev[m];
     }
-
-    
-
     
 
     
