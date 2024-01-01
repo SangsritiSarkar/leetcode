b1c71d9af3f5ae050e570d9525a94cfd9c4f3df1
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2024-01-02 00:14:33 +0530

    Time: 211 ms (10.19%) | Memory: 7.1 MB (93.46%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -4,9 +4,9 @@ public:
     bool isMatch(string s2, string s1) {
         int n=s1.size();
         int m=s2.size();
-        vector<vector<bool>> dp(n+1, vector<bool> (m+1,false));
-        dp[0][0]=true;;
-        for(int j=1;j<=m;j++) dp[0][j]=false;
+        vector<bool> prev(m+1,false), curr(m+1,false);
+        prev[0]=true;;
+        //for(int j=1;j<=m;j++) dp[0][j]=false;
         for(int i=1;i<=n;i++)
         {
             int flag=true;
@@ -14,22 +14,19 @@ public:
             {
                 if(s1[ii-1]!='*') {flag=false; break;}
             }
-            dp[i][0]=flag;
-
-        }
-        for(int i=1;i<=n;i++)
-        {
+            curr[0]=flag;
             for(int j=1;j<=m;j++)
             {
                 if(s1[i-1]==s2[j-1] || s1[i-1]=='?')
-                   dp[i][j]=dp[i-1][j-1];
+                   curr[j]=prev[j-1];
                 else if(s1[i-1]=='*')
-                      dp[i][j]=dp[i-1][j] || dp[i][j-1];
+                      curr[j]=prev[j] ||curr[j-1];
                 else
-                      dp[i][j]=false;  
+                      curr[j]=false;  
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
     
     
