6841dcacac2729dac4a4cb4d96640390635deceb
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2024-01-01 21:57:38 +0530

    Time: 59 ms (21.96%) | Memory: 6.7 MB (92.01%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -43,26 +43,22 @@ public:
 
     // } 
 
-//SPACE OPTIMIZATION   
+//1D ARRAY SPACE OPTIMIZATION   
     
     int numDistinct(string s1, string s2) {
         int n=s1.size();
         int m=s2.size();
-        vector<double> prev(m+1,0), curr(m+1,0);
-        prev[0]=curr[0]=1;
+        vector<double> prev(m+1,0);
+        prev[0]=1;
         //for(int j=1;j<=m;j++) dp[0][j]=0;
         for(int i=1;i<=n;i++)
         {
-            for(int j=1;j<=m;j++)
+            for(int j=m;j>=1;j--)
             {
                 if(s1[i-1]==s2[j-1])
-                    curr[j]= prev[j-1]+prev[j];
-            else
-                curr[j]=prev[j]; 
+                    prev[j]= prev[j-1]+prev[j];
             }
-            prev=curr;
         }
-        
         return (int)prev[m];
 
     }  
