59e8df8d33a77e2d1b617b75a4b5d18ffe624c17
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2024-01-04 22:23:23 +0530

    Time: 334 ms (32.30%) | Memory: 11.1 MB (32.38%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-//TABULATION
+//SPACE OPTIMIZATION
     
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(n+1, vector<int> (n+1,0));
+        vector<int> next(n+1,0), curr(n+1,0);
         for(int ind=n-1;ind>=0;ind--)
         {
             for(int prev=ind-1;prev>=-1;prev--)
             {
-                int notTake=0+dp[ind+1][prev+1];
+                int notTake=0+next[prev+1];
                 int take=0;
                 if(prev==-1 || nums[ind]>nums[prev])
-                   take=1+dp[ind+1][ind+1]; //COORDINATE SHIFT PREV part
-                dp[ind][prev+1]=max(take, notTake);
+                   take=1+next[ind+1]; //COORDINATE SHIFT PREV part
+                curr[prev+1]=max(take, notTake);
             }
+            next=curr;
             
         }
-        return dp[0][0];
+        return next[0];
        
     }
 };
