04d2feadf0a9b75918979c6a028346887bfd39a5
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2024-01-04 22:19:38 +0530

    Time: 493 ms (27.37%) | Memory: 292.5 MB (16.37%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-//MEMOIZATION
-    int f(int ind, int prev, int n, vector<int>& nums, vector<vector<int>>& dp)
-    {
-        if(ind==n) return 0;
-        if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
-        int notTake=0+f(ind+1,prev,n,nums,dp);
-        int take=0;
-        if(prev==-1 || nums[ind]>nums[prev])
-           take=1+f(ind+1,ind,n,nums,dp);
-        return dp[ind][prev+1]=max(take,notTake);   
-    }
-
-   
+//TABULATION
+    
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(n, vector<int> (n+1,-1));
-        return f(0,-1,n,nums,dp);
+        vector<vector<int>> dp(n+1, vector<int> (n+1,0));
+        for(int ind=n-1;ind>=0;ind--)
+        {
+            for(int prev=ind-1;prev>=-1;prev--)
+            {
+                int notTake=0+dp[ind+1][prev+1];
+                int take=0;
+                if(prev==-1 || nums[ind]>nums[prev])
+                   take=1+dp[ind+1][ind+1]; //COORDINATE SHIFT PREV part
+                dp[ind][prev+1]=max(take, notTake);
+            }
+            
+        }
+        return dp[0][0];
        
     }
 };
