382c0a28b13db45cc041554545c4642ff41621ad
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2024-01-04 23:56:10 +0530

    Time: 237 ms (58.53%) | Memory: 11 MB (36.75%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-//SPACE OPTIMIZATION
-    
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> next(n+1,0), curr(n+1,0);
-        for(int ind=n-1;ind>=0;ind--)
-        {
-            for(int prev=ind-1;prev>=-1;prev--)
-            {
-                int notTake=0+next[prev+1];
-                int take=0;
-                if(prev==-1 || nums[ind]>nums[prev])
-                   take=1+next[ind+1]; //COORDINATE SHIFT PREV part
-                curr[prev+1]=max(take, notTake);
-            }
-            next=curr;
-            
-        }
-        return next[0];
        
+        int n=nums.size(), maxi=1;
+        vector<int> dp(n,1), hash(n,1);
+        for(int i=0;i<n;i++)
+        {
+            for(int prev=0; prev<i;prev++)
+            {
+                if(nums[prev]<nums[i])
+                {
+                    dp[i]=max(dp[i], 1+dp[prev]);
+                }
+            }
+            if(dp[i]>maxi)
+               maxi=dp[i];
+        }
+        return maxi;
     }
 };
