3be28f0a594cfc1074f63bae15b207ce51d17d1e
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2024-01-04 22:06:26 +0530

    Time: 841 ms (16.62%) | Memory: 292.7 MB (6.56%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+//TABULATION
+    int f(int ind, int prev, int n, vector<int>& nums, vector<vector<int>>& dp)
+    {
+        if(ind==n) return 0;
+        if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
+        int notTake=0+f(ind+1,prev,n,nums,dp);
+        int take=0;
+        if(prev==-1 || nums[ind]>nums[prev])
+           take=1+f(ind+1,ind,n,nums,dp);
+        return dp[ind][prev+1]=max(take,notTake);   
+    }
+
+   
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> dp(n, vector<int> (n+1,-1));
+        return f(0,-1,n,nums,dp);
+       
+    }
+};
