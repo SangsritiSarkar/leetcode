f3e02c0c4a3f2225719c2b09082f4093d7af5302
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2024-01-07 03:00:04 +0530

    Time: 171 ms (68.38%) | Memory: 72.5 MB (68.45%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int getNextInd(vector<vector<int>> &arr, int ind, int currET, int n)
     {
@@ -7,32 +7,7 @@ public:
         })-begin(arr);
         
     }
-    // int getNextInd(vector<vector<int>> &arr, int ind, int currET, int n)
-    // {
-    //     int h=n-1;
-    //     int l=0, ans=n+1;
-    //     while(l<=h)
-    //     {
-    //         int mid=(l+h)/2;
-    //         if(arr[mid][0]>=currET)
-    //         {
-    //             ans=mid; h=mid-1;
-    //         }
-    //         else l=mid+1;
-    //     }
-    //     return ans;
-        
-    // }
-    int f(int ind, vector<vector<int>> &arr, int n, vector<int>& dp)
-    {
-       if(ind>=n) return 0;
-       if(dp[ind]!=-1) return dp[ind];
-       int nextInd=getNextInd(arr,ind+1,arr[ind][1],n);
-       
-       int take=arr[ind][2] +f(nextInd,arr,n,dp);
-       int notTake=0 + f(ind+1 , arr, n,dp);
-       return dp[ind]=max(take, notTake);
-    }
+    
     int jobScheduling(vector<int>& st, vector<int>& et, vector<int>& profit) {
         
         int n=profit.size();
@@ -40,13 +15,7 @@ public:
         
         for(int i=0;i<n;i++)
         {
-            // vector<int> temp;
-            // for(int j=0;j<3;j++)
-            // {
-            //     temp={st[j],et[j],profit[j]};//RUNTIME ERROR
             
-            // }
-            //arr.push_back(temp);
             arr[i][0]=st[i];
             arr[i][1]=et[i];
             arr[i][2]=profit[i];
@@ -55,8 +24,57 @@ public:
            return a[0]<=b[0];
         };
         sort(arr.begin(),arr.end());
-        vector<int> dp(n,-1);
-        return f(0,arr,n,dp);
+        vector<int> dp(n,0);
+        for(int ind=n-1;ind>=0;ind--)
+        {
+            int nextInd=getNextInd(arr,ind+1,arr[ind][1],n);
+            int take=arr[ind][2] +dp[nextInd];
+            int notTake=0 + dp[ind+1];
+            dp[ind]=max(take, notTake);
+
+        }
+        return dp[0];
+    }
+};
+*/
+
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int getNextInd(vector<vector<int>> &arr, int ind, int currET, int n) {
+        return lower_bound(begin(arr) + ind, end(arr), currET, [&](const auto& a, int val) {
+            return a[0] < val;
+        }) - begin(arr);
+    }
+
+    int jobScheduling(vector<int>& st, vector<int>& et, vector<int>& profit) {
+        int n = profit.size();
+        vector<vector<int>> arr(n, vector<int>(3, 0));
+
+        for (int i = 0; i < n; i++) {
+            arr[i][0] = st[i];
+            arr[i][1] = et[i];
+            arr[i][2] = profit[i];
+        }
+
+        auto compa = [&](vector<int>& a, vector<int>& b) {
+            return a[0] <= b[0];
+        };
+        sort(arr.begin(), arr.end(), compa);
+
+        vector<int> dp(n, 0);
+        for (int ind = n - 1; ind >= 0; ind--) {
+            int nextInd = getNextInd(arr, ind + 1, arr[ind][1], n);
+            int take = arr[ind][2] + ((nextInd < n) ? dp[nextInd] : 0);
+            int notTake = (ind + 1 < n) ? dp[ind + 1] : 0;
+            dp[ind] = max(take, notTake);
+        }
+
+        return dp[0];
     }
 };
 
