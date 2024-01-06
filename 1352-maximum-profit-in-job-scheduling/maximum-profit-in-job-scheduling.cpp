8fc20a60583dfe70cb5cd6fb4ffd74c9a31848c7
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2024-01-07 03:02:36 +0530

    Time: 189 ms (56.90%) | Memory: 72.6 MB (67.22%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -1,4 +1,4 @@
-/*class Solution {
+class Solution {
 public:
     int getNextInd(vector<vector<int>> &arr, int ind, int currET, int n)
     {
@@ -24,7 +24,7 @@ public:
            return a[0]<=b[0];
         };
         sort(arr.begin(),arr.end());
-        vector<int> dp(n,0);
+        vector<int> dp(n+1,0);
         for(int ind=n-1;ind>=0;ind--)
         {
             int nextInd=getNextInd(arr,ind+1,arr[ind][1],n);
@@ -36,47 +36,9 @@ public:
         return dp[0];
     }
 };
-*/
 
-#include <vector>
-#include <algorithm>
 
-using namespace std;
 
-class Solution {
-public:
-    int getNextInd(vector<vector<int>> &arr, int ind, int currET, int n) {
-        return lower_bound(begin(arr) + ind, end(arr), currET, [&](const auto& a, int val) {
-            return a[0] < val;
-        }) - begin(arr);
-    }
-
-    int jobScheduling(vector<int>& st, vector<int>& et, vector<int>& profit) {
-        int n = profit.size();
-        vector<vector<int>> arr(n, vector<int>(3, 0));
-
-        for (int i = 0; i < n; i++) {
-            arr[i][0] = st[i];
-            arr[i][1] = et[i];
-            arr[i][2] = profit[i];
-        }
-
-        auto compa = [&](vector<int>& a, vector<int>& b) {
-            return a[0] <= b[0];
-        };
-        sort(arr.begin(), arr.end(), compa);
-
-        vector<int> dp(n, 0);
-        for (int ind = n - 1; ind >= 0; ind--) {
-            int nextInd = getNextInd(arr, ind + 1, arr[ind][1], n);
-            int take = arr[ind][2] + ((nextInd < n) ? dp[nextInd] : 0);
-            int notTake = (ind + 1 < n) ? dp[ind + 1] : 0;
-            dp[ind] = max(take, notTake);
-        }
-
-        return dp[0];
-    }
-};
 
 
 
