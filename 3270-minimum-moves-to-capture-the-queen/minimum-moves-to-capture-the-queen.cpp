30e4cc8c6c584e58911b75d712f88b4b197b56b5
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2024-01-07 20:11:35 +0530

    Time: 4 ms (75.00%) | Memory: 6.3 MB (100.00%) - LeetSync

--- 3270-minimum-moves-to-capture-the-queen/minimum-moves-to-capture-the-queen.cpp
+++ 3270-minimum-moves-to-capture-the-queen/minimum-moves-to-capture-the-queen.cpp
@@ -3,24 +3,19 @@ public:
    
     
     int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
-        // if(a==e || b==f)
-        // {
-        //     if(a==e && a==c && (d-b)*(d-f)<0) return 2;
-        //     if(b==f && b==d && (c-a)*(c-e)<0) return 2;
-        //     return 1;
-        // }
-        // if(abs(c-e)==abs(d-f) && abs(c-a)==abs(d-b) && abs(a-e)==abs(b-f) && ((b-d)*(b-f)<0)) return 1;
-        // return 2;
-
-        if(a == e || b == f) {
-            if(a == e && a == c && ((d-b) * (d-f) < 0)) return 2;
-            if(b == f && b == d && ((c-a) * (c-e) < 0)) return 2;
+        if(a==e || b==f)
+        {
+            if(a==e && a==c && (d-b)*(d-f)<0) return 2;
+            if(b==f && b==d && (c-a)*(c-e)<0) return 2;
             return 1;
         }
-        if(abs(c - e) == abs(d - f)) {
-            if(abs(c - a) == abs(d - b) && abs(e - a) == abs(f - b) && ((b-f) * (b-d) < 0))  return 2;
+        if(abs(c-e)==abs(d-f))
+        {
+            if(abs(c-e)==abs(d-f) && abs(c-a)==abs(d-b) && abs(a-e)==abs(b-f) && ((b-d)*(b-f)<0)) return 2;
             return 1;
         }
         return 2;
+
+        
     }
 };
