0a284f4b5c13e0464abb1fdeecc36c6ec2f19e72
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2024-01-31 01:07:43 +0530

    Time: 9 ms (55.52%) | Memory: 15.4 MB (16.35%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 class Solution {
 public:
-    int op(int n1, int n2, char opt)
+    int op(int n1, int n2, string opt)
     {
-        if(opt=='+') return n1+n2;
-        else if(opt=='-') return n1-n2;
-        else if(opt=='/') return n1/n2;
+        if(opt=="+") return n1+n2;
+        else if(opt=="-") return n1-n2;
+        else if(opt=="/") return n1/n2;
         else return n1*n2;
     }
     int evalRPN(vector<string>& tokens) {
@@ -14,13 +14,14 @@ public:
         
         for(int i=0;i<n;i++)
         {
-            if(tokens[i].size()==1 && tokens[i][0]<48)
+            //if(tokens[i].size()==1 && tokens[i][0]<48)
+            if(tokens[i]=="+" || tokens[i]=="-" || tokens[i]=="*" || tokens[i]=="/")
             {
                 int x2=st.top();
                 st.pop();
                 int x1=st.top();
                 st.pop();
-                st.push(op(x1,x2,tokens[i][0]));
+                st.push(op(x1,x2,tokens[i]));
             }
             else st.push(stoi(tokens[i]));
         }
