e8141461b844d11da2114cd0fc7fc0c0eb37f32e
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2024-07-10 01:34:03 +0530

    Time: 148 ms (65.01%) | Memory: 76.6 MB (58.31%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& c) {
-        double t=0;
-        double tt=0;//=c[0][1];
-        double st=0;//=c[0][0]+c[0][1];
+        double t=0,tt=0,st=0,a,ck;
         for(int i=0; i<c.size();++i)
         {
-            double a=c[i][0];
+            a=c[i][0];
+            ck=c[i][1];
             if(a>st) {tt+=c[i][1]; st=a+c[i][1];}
-            else {t=c[i][1]+st;
-             st=t;
-             tt+=t-a;}
+            else {tt+=ck+st-a;
+             st+=ck;
+             }
         }
         double ans=tt/c.size();
         return ans;
