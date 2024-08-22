52e27607919e04f22e28cdb300b3e77a284b3e88
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2024-08-22 09:38:35 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (29.89%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -6,14 +6,16 @@ public:
         while(l<=h){
             int mid=(l+h)/2;
             if(arr[mid]==target) return mid;
+            //left half is sorted
             else if(arr[mid]>=arr[l]){
                 if(arr[l]<=target and target<arr[mid]) h=mid-1;
-                else l=mid+1;
+                else l=mid+1;//eliminate left half
             }
+            //right half is sorted
             else
             {
                 if(arr[mid]<target and target<=arr[h]) l=mid+1;
-                else h=mid-1;
+                else h=mid-1; //eliminate right half
             }
         }
         return -1;
