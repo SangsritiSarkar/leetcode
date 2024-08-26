61dd5b1978cf25a3df2bc8c28be548f999ed1bce
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2024-08-27 00:31:59 +0530

    Time: 7 ms (35.11%) | Memory: 23.4 MB (20.28%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n=s.size();
+        string temp="",ans="";
+        
+        for(int i=0;i<n;i++){
+            if(s[i]!=' '){
+                temp+=s[i];
+            }
+            else if(s[i]==' '){
+                  if(i==n-1 or i==0) continue; //ignore start and end space
+                  else if(s[i-1]==' ' and ans!="") continue;
+                  else if(ans!="") ans=temp+" "+ans; //not space even before beginning
+                  
+                  else ans=temp;
+                  temp="";
+            }
+        }
+        cout<<temp;
+        if(temp!="") {
+            if(ans!="") ans=temp+' '+ans;
+            else ans=temp;
+        }
+        return ans;
+    }
+};
