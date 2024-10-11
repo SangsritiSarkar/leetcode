78c9009bdbc2349e33437f8e3b460266b6f60be0
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2024-10-12 01:49:05 +0530

    Time: 22 ms (28.26%) | Memory: 18 MB (63.41%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,53 +1,33 @@
 class Solution {
-private: 
-
-    bool dfs(int node, vector<int> adj[], int vis[], int pathVis[]){
-        vis[node] = 1;
-        pathVis[node] = 1;
-
-        for(auto it:adj[node]){
-            if(!vis[it]){
-                if(dfs(it,adj,vis,pathVis)==true) return true;
-            }
-            else if(pathVis[it]){
-                return true;
-            }
-        }
-
-        pathVis[node]=0;
-        return false;
-
-    }
-
 public:
-    
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        if(prerequisites.size()==1) return true;
+    bool canFinish(int numCourses, vector<vector<int>>& prereq) {
+        int n=prereq.size();
         vector<int> adj[numCourses];
-        for(int i=0;i<prerequisites.size();i++){
-            int u=prerequisites[i][0];
-            int v=prerequisites[i][1];
+        for(int i=0;i<n;++i){
+            int u=prereq[i][0];
+            int v=prereq[i][1];
 
-            adj[v].push_back(u);
-            
+            adj[u].push_back(v);
         }
 
-        int vis[numCourses];
-        int pathVis[numCourses];
+        vector<int> indegree(numCourses,0);
+        for(int i=0;i<numCourses;++i) {
+            for(auto it:adj[i]) indegree[it]++;
+        }
 
-        for(int i=0;i<numCourses;i++){
-            vis[i] = 0;
-           
-        }  
-        
-        for(int i=0;i<numCourses;i++)
-        {
-            if(!vis[i]){
-                if(dfs(i,adj,vis,pathVis)==true) return false;
+        queue<int> q;
+        for(int i=0;i<numCourses;++i) if(indegree[i]==0) q.push(i);
+        int cnt=0;
+
+        while(!q.empty()){
+            int node=q.front();
+            q.pop(); cnt++;
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
             }
         }
-        return true;
-
-   }
-
+        if(cnt==numCourses) return true;
+        return false;
+    }
 };
