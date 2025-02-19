09b72dc55dd03610a427d5491401a8819150d2ee
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2025-02-19 14:50:44 +0530

    Time: 2 ms (35.59%) | Memory: 17 MB (41.25%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,43 +1,50 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        int nr=grid.size();
-        int nc=grid[0].size();
-        queue<pair<pair<int,int>,int>> q;
-        for(int i=0;i<nr;i++)
-        {
-            for(int j=0;j<nc;j++)
-            {
-                if(grid[i][j]==2) q.push({{i,j},0});
-            }
-        }
-        int ans=0;
-        while(!q.empty())
-        {
+    void bfs(int tr, int tc, vector<vector<int>> &grid , queue<pair<pair<int,int>, int>> &q, int &ans){
+        
+        
+        vector<int> delrow={-1,0,0,1};
+        vector<int> delcol={0,-1,1,0};
+        while(!q.empty()){
             auto node=q.front();
-            int row=node.first.first;
-            int col=node.first.second;
+            int r=node.first.first;
+            int c=node.first.second;
             int time=node.second;
+            
+            
             q.pop();
-            int r[]={0,1,0,-1};
-            int c[]={-1,0,1,0};
-            for(int i=0;i<4;i++)
+            for(int i=0;i<4;++i)
             {
-                int delrow=row+r[i];
-                int delcol=col+c[i];
-                if(delrow>=0 and delrow<nr and delcol>=0 and delcol<nc and grid[delrow][delcol]==1)
-                {
-                    q.push({{delrow,delcol},time+1});
-                    grid[delrow][delcol]=2;
+                int newr=r+delrow[i], newc=c+delcol[i];
+                if(newr>=0 and newr<tr and newc>=0 and newc<tc and grid[newr][newc]==1)            {
+                    q.push({{newr,newc},time+1});
+                    grid[newr][newc]=2;
                     ans=max(ans,time+1);
                 }
             }
         }
-        for(int i=0;i<nr;i++)
+
+        for(auto it: grid)
         {
-            for(int j=0;j<nc;j++) 
-               if(grid[i][j]==1) return -1;
+            for(auto j:it) if(j==1) {ans=-1; break;}
         }
+
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        int nr=grid.size(), nc=grid[0].size(), ans=0;
+       
+        queue<pair<pair<int,int>, int>> q;
+        for(int i=0;i<nr;++i)
+        {
+            for(int j=0;j<nc;++j)
+            {
+                if(grid[i][j]==2){
+                    q.push({{i,j},0});
+                    
+                }
+            }
+        }
+        bfs(nr,nc, grid, q, ans);
         return ans;
     }
 };
