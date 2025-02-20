74754e75de01cad5197c4c21b8c960ae83e318b0
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2025-02-20 15:33:02 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (48.56%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int nr=image.size();
-        int nc =image[0].size();
-        int origin=image[sr][sc];
-        if(origin==color) return image;
-        image[sr][sc]=color;
         queue<pair<int,int>> q;
+        int col=image[sr][sc];
+        if(col==color) return image;
+        image[sr][sc]=color;
         q.push({sr,sc});
-        while(!q.empty())
-        {
-            int row=q.front().first;
-            int col=q.front().second;
+        vector<int> r={-1,0,0,1};
+        vector<int> c={0,-1,1,0};
+        int nr=image.size();
+        int nc=image[0].size();
+
+        while(!q.empty()){
+            auto node=q.front();
+            int tr=node.first;
+            int tc=node.second;
             q.pop();
-            int r[]={-1,0,1,0};
-            int c[]={0,-1,0,1};
-            for(int i=0;i<4;i++)
-            {
-                int delrow=row+r[i];
-                int delcol=col+c[i];
-                if(delrow>=0 and delrow<nr and delcol>=0 and delcol<nc and  image[delrow][delcol]==origin)
-                {
-                    q.push({delrow,delcol});
+            for(int i=0;i<4;++i){
+                int delrow=tr+r[i];
+                int delcol=tc+c[i];
+                if(delrow>=0 and delrow<nr and delcol>=0 and delcol<nc and image[delrow][delcol]==col){
                     image[delrow][delcol]=color;
+                    q.push({delrow,delcol});
                 }
             }
         }
