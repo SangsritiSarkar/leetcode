21f30d93d81effa0e6b6aef1359055804d972bc9
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2025-02-26 13:33:13 +0530

    Time: 32 ms (22.52%) | Memory: 38.9 MB (34.40%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,51 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-      int n=mat.size();
-      int m=mat[0].size();
-      queue<pair<pair<int,int>,int>> q;
-      vector<vector<int>> vis(n,vector<int> (m,0));
-      vector<vector<int>> dist(n,vector<int> (m,0));
-      int l=0;
-      for(int i=0;i<n;i++)
-      {
-        for(int j=0;j<m;j++)
-        {
-            if(mat[i][j]==0)
-            {
-                q.push({{i,j},l});
-                vis[i][j]=1;
-                //dist[i][j]=0;
+        int nr=mat.size();
+        int nc=mat[0].size();
+        int l=0;
+        vector<vector<int>> vis(nr, vector<int> (nc));
+        vector<vector<int>> dis(nr, vector<int> (nc));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0;i<nr;++i){
+            for(int j=0;j<nc;++j){
+                if(mat[i][j]==0) {
+                    q.push({{i,j},l});
+                    vis[i][j]=1;
+                }
             }
         }
-      }
-       int row[]={-1,0,1,0};
-        int col[]={0,-1,0,1};
-      while(!q.empty())
-      {
-        auto it=q.front();
-        q.pop();
-        int r=it.first.first;
-        int c=it.first.second;
-        int l=it.second;
-        //dist[r][c]=l;
-       
-        
-        for(int i=0;i<4;i++)
-        {
-            
+
+        vector<int> row={-1,0,1,0};
+        vector<int> col={0,-1,0,1};
+
+        while(!q.empty()){
+            auto node=q.front();
+            int r=node.first.first;
+            int c=node.first.second;
+            int lev=node.second;
+            dis[r][c]=lev;
+            q.pop();
+
+            for(auto i=0;i<4;i++)
+            {
                 int delrow=r+row[i];
                 int delcol=c+col[i];
-                if(delrow>=0 and delrow<n and delcol>=0 and delcol<m and !vis[delrow][delcol])
-                {
-                   q.push({{delrow,delcol},l+1});
-                   vis[delrow][delcol]=1;
-                   dist[delrow][delcol]=l+1;
-                }
 
-            
+                if(delrow>=0 and delrow<nr and delcol>=0 and delcol<nc and !vis[delrow][delcol]){
+                    q.push({{delrow,delcol},lev+1});
+                    vis[delrow][delcol]=1;
+                }
+            }
         }
-      }
-      return dist;
+        return dis;
     }
 };
