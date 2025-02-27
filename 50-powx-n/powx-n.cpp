5eaba14ab2abc2ff724bb4f1cf58c11f1d0bdf3b
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2025-02-27 15:35:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (71.46%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -3,7 +3,7 @@ public:
     
     double myPow(double x, int n) {
         long long nn=n;
-        if(nn<0) nn=-nn;
+        if(nn<0) nn=nn*-1;
         double ans= 1;
         while(nn){
             if(nn&1){
