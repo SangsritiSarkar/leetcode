bcf783e653abbe0d4fb1e8823dfa029760d7764c
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2025-02-27 15:34:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (92.59%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    double func(double x,long long n){
-       
-        if(n==1) return x;
-        double half=func(x,n/2);
-        if(n&1){
-            return half*half*x;
-        }
-        else return half*half;
-    }
+    
     double myPow(double x, int n) {
         long long nn=n;
-        if(nn==0) return 1;       
-        else if(nn>0) return func(x,nn);
-        return 1.0/func(x,-nn);
+        if(nn<0) nn=-nn;
+        double ans= 1;
+        while(nn){
+            if(nn&1){
+                ans*=x;
+                nn--;
+            }
+            else {x*=x; nn/=2;}
+        }       
+        if(n<0) return (double)(1/ans);
+        return ans;
     }
     
 };
