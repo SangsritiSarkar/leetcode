a0390f968d19ec3f7defcd413445935c569ff618
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2025-04-16 12:04:21 +0530

    Time: 43 ms (70.73%) | Memory: 47.9 MB (68.63%) - LeetSync

--- 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.cpp
+++ 1120-flower-planting-with-no-adjacent/flower-planting-with-no-adjacent.cpp
@@ -8,18 +8,18 @@ public:
         return true;
     }
 
-    bool solve(int node, int n, vector<int> &c, vector<vector<int>> &adj){
+    void solve(int node, int n, vector<int> &c, vector<vector<int>> &adj){
         if(node==n+1){
-            return true;
+            return ;
         }
         for(int flower=1; flower<=4; ++flower){
             if(possible(node, flower,c, adj)){
                 c[node-1]=flower;
-                if(solve(node+1,n,c,adj)) return true;
-                c[node-1]=0;
+                solve(node+1,n,c,adj);
+                break;//no bcktracking required becoz a sol exist
             }
         }
-        return false;
+        
     }
 
     vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
@@ -31,7 +31,7 @@ public:
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
         }
-        if(!solve(1, n, c, adj)) return {};
+        solve(1, n, c, adj);
         return c;
 
 
