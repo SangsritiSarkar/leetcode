d0b060ee85d9080749c5aad4990623e0420a527c
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2025-09-18 00:56:29 +0530

    Time: 3 ms (40.17%) | Memory: 14 MB (59.98%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    void solve(int ind, int target, vector<int> &ds, vector<vector<int>> &ans, vector<int> &cand){
-        if(target==0){
-            ans.push_back(ds);
-            return;
-        }
-        for(int i=ind;i<cand.size();++i){
-            if(i!=ind and cand[i]==cand[i-1]) continue; //if i==ind it is okay
-            if(target>=cand[i]) {
+    void func(int ind, int target, vector<int> &ds, vector<vector<int>> &ans, vector<int> &cand){
+        if(target==0) {ans.push_back(ds); return;}
+        for(int i=ind; i<cand.size(); ++i){
+            if(i>ind and cand[i]==cand[i-1]) continue;
+            if(cand[i]<=target){
                 ds.push_back(cand[i]);
-                solve(i+1, target-cand[i], ds, ans, cand);
+                func(i+1, target-cand[i], ds,ans,cand);
                 ds.pop_back();
             }
-
         }
-
     }
-    vector<vector<int>> combinationSum2(vector<int>& cand, int target) {
-        sort(cand.begin(), cand.end());
-        //set<vector<int>> st;
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
         vector<vector<int>> ans;
         vector<int> ds;
-        solve(0,target, ds, ans, cand);
+        func(0,target,ds,ans,candidates);
         return ans;
     }
 };
