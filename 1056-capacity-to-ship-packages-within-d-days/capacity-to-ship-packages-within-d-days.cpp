82b13a161523c4a2c8d837341b54b9025939cfce
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2026-09-20 04:26:55 +0530

    Time: 11 ms (65.59%) | Memory: 34.9 MB (87.75%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int func(vector<int>& weights, int w){
-        int d=1, curr=0;
+    int possible(vector<int>& weights, int cap, int d){
+        int tot=0, cnt=1;
         for(auto it:weights){
-            if(it+curr<=w) curr+=it;
+            if(tot+it<=cap){
+                tot+=it;
+            }
             else{
-                d++;
-                curr=it;
+                cnt++;
+                tot=it;
             }
         }
-        return d;
+        if(cnt<=d) return true;
+        return false;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        int n=weights.size();
-        int low=*max_element(weights.begin(),weights.end());
-        int high=accumulate(weights.begin(),weights.end(),0);
+        int low=INT_MIN, high=0,mid;
+        for(auto it:weights) {high+=it;low=max(low,it);}
         while(low<=high){
-            int mid=(low+high)>>1;
-            int cnt=func(weights,mid);
-            if(cnt>days) low=mid+1;
-            else high=mid-1;
+            mid=low+(high-low)/2;
+            if(possible(weights, mid, days)) high=mid-1;
+            else low=mid+1;
         }
         return low;
     }
