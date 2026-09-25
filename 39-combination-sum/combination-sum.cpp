1811ff12643e9f07c179846a2e6de4f7b49078cc
SangsritiSarkar <96903468+SangsritiSarkar@users.noreply.github.com>
2026-09-26 01:56:52 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (92.93%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-
-    void func(int ind, int target, vector<vector<int>> &ans, vector<int> &cand, vector<int> &ds){
-        if(ind==cand.size()){
+    void func(int ind, vector<int> &ds, int target, vector<int> &arr, vector<vector<int>> &ans){
+        if(ind==arr.size()){
             if(target==0) ans.push_back(ds);
             return;
         }
-        if(cand[ind]<=target){
-            ds.push_back(cand[ind]);
-            func(ind, target-cand[ind], ans, cand, ds);
+        if(arr[ind]<=target){
+            ds.push_back(arr[ind]);
+            func(ind,ds, target-arr[ind], arr, ans);
             ds.pop_back();
         }
-        func(ind+1, target, ans,cand,ds);
+        func(ind+1,ds, target, arr, ans);
     }
+
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n=candidates.size();
         vector<vector<int>> ans;
         vector<int> ds;
-        func(0,target,ans,candidates,ds);
+        func(0, ds,target, candidates, ans);
         return ans;
     }
 };
